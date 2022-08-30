78afbd7083fde951afaefc3961a3879931f24779
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-08-29 23:14:26 -0700

    remaps for oldfiles and buffers

--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -1,20 +1,13 @@
 local nnoremap = require("setup.keymap").nnoremap
 
+-- TODO refactor remaps to be native lua, maybe get rid of keymap? 
+
 -- change theme
 nnoremap(
   '<leader>mm',
   [[<Cmd>lua require('material.functions').toggle_style()<CR>]],
   { noremap = true, silent = true }
 )
-
---[=====[ 
--- coc
-vim.cmd([[
-  inoremap <silent><expr> <cr> coc#pum#visible() ? coc#_select_confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-  inoremap <expr> <cr> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
-  inoremap <expr> <tab> coc#pum#visible() ? coc#pum#confirm() : "\<tab>"
-]])
---]=====]
 
 -- telescope
 nnoremap(
@@ -34,6 +27,16 @@ nnoremap(
 nnoremap(
   '<leader>gr',
   [[<cmd>lua require('telescope.builtin').live_grep()<cr>]]
+)
+
+nnoremap(
+  '<leader>b',
+  '<cmd>Telescope buffers<cr>'
+)
+
+nnoremap(
+  '<leader>o',
+  '<cmd>Telescope oldfiles<cr>'
 )
 
 -- lsp
