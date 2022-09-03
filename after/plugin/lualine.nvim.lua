1ecec5ee72bd640cec0ce581dfe1dbbc8a9adb56
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-09-02 20:50:56 -0700

    stuff

--- after/plugin/lualine.nvim.lua
+++ after/plugin/lualine.nvim.lua
@@ -6,7 +6,7 @@ require('lualine').setup {
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
-      statusline = {},
+      statusline = {'NvimTree'},
       winbar = {},
     },
     ignore_focus = {},
@@ -29,13 +29,29 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = {'filename', 'diff'},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
+  tabline = {
+    lualine_a = {
+      { 'buffers',       
+      symbols = {
+        modified = ' *',      -- Text to show when the buffer is modified
+        alternate_file = '', -- Text to show to identify the alternate file
+        directory =  '',     -- Text to show when the buffer is a directory
+      },
+    }},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {'tabs'}
+  },
   winbar = {},
   inactive_winbar = {},
   extensions = {}
 }
+
+
