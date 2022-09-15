9baf1329f83b46b83f4f60fc3a9a79f7e7afca3d
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-09-14 22:29:55 -0700

    changed script to handle when already in tmux

--- scripts/tmux-session.sh
+++ scripts/tmux-session.sh
@@ -2,8 +2,23 @@
 
 dir=${PWD##*/}
 
-if tmux has-session -t ${dir} 2>/dev/null; then
-  tmux a -t ${dir}
+if [[ -z $TMUX ]]; then
+  if tmux has-session -t ${dir} 2>/dev/null; then
+    printf "attaching to existing session\n"
+    tmux a -t ${dir}
+  else
+    printf "creating new session\n"
+    tmux new -s ${dir}\; send-keys "nvim ." ENTER
+  fi
+
 else
-  tmux new -s ${dir}\; send-keys "nvim ." ENTER
+  if tmux has-session -t ${dir} 2>/dev/null; then
+    printf "switching to existing session\n"
+    tmux switch -t ${dir}\;
+  else
+    printf "creating new session\n"
+    tmux new -A -s ${dir} -d
+    printf "attaching to new session\n"
+    tmux switch -t ${dir}\; send-keys "nvim ." ENTER
+  fi
 fi
