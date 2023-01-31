edf6516478f6081eab03a8ccb23a1e0bc2bb0546
b. vaughn sulit <bvaughnsulit@gmail.com>
2023-01-30 20:31:23 -0800

    deleted custom theme functionality

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,6 @@
-local setup = function(opts)
-  -- if opts.colorscheme ~= nil then
-  --   vim.cmd('colorscheme ' .. opts.colorscheme)
-  -- end
-
-  require 'config.lazy'
-  require 'config.settings'
-
-  require 'config.autocmds'
-  require 'config.functions'
-  require 'config.keymaps'
-  -- require 'config.theme'
-  require 'config.tmux'
-end
-
--- called on initial startup
-setup { colorscheme = 'minischeme' }
-
--- can be called later to reload config
-vim.api.nvim_create_user_command('ChangeTheme', function(opts)
-  local setup_opts = {}
-  if opts.args ~= '' then
-    setup_opts = { colorscheme = opts.fargs[1] }
-  end
-  setup(setup_opts)
-end, { complete = 'color', nargs = '*' })
+require 'config.lazy'
+require 'config.settings'
+require 'config.autocmds'
+require 'config.functions'
+require 'config.keymaps'
+require 'config.tmux'
--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -1,68 +1,3 @@
--- set custom word highlights for material
-require('material').setup {
-  custom_highlights = {
-    MiniCursorword = { link = 'Search' },
-    MiniCursorwordCurrent = { link = 'Search' },
-  },
-}
-
-vim.keymap.set(
-  'n',
-  '<leader>mm',
-  [[<Cmd>lua require('material.functions').toggle_style()<CR>]],
-  { noremap = true, silent = true }
-)
-
--- browse themes
-vim.api.nvim_create_user_command('ThemeBrowse', function()
-  require('telescope.builtin').colorscheme {
-    layout_config = {
-      height = 0.5,
-      preview = false,
-      width = 0.2,
-    },
-    enable_preview = true,
-  }
-end, {})
-
-local themes = {}
-
-themes.miniScheme = function()
-  vim.cmd 'colorscheme minischeme'
-end
-vim.api.nvim_create_user_command('MiniScheme', themes.miniScheme, {})
-
--- themes.gruvboxLight = function ()
---   vim.g.gruvbox_material_background = 'medium'
---   vim.opt.background=light
---   vim.cmd('colorscheme gruvbox-material')
--- end
--- vim.api.nvim_create_user_command('GruvboxLight', themes.gruvboxLight, {})
-
-themes.materialDeepOcean = function()
-  vim.g.material_style = 'deep ocean'
-  vim.cmd 'colorscheme material'
-end
-vim.api.nvim_create_user_command('MaterialDeepOcean', themes.materialDeepOcean, {})
-
-themes.monokai = function()
-  vim.g.monokaipro_filter = 'classic'
-  vim.cmd 'colorscheme monokaipro'
-end
-vim.api.nvim_create_user_command('Monokai', themes.monokai, {})
-
-themes.catppuccin = function()
-  vim.g.catppuccin_flavour = 'mocha'
-  vim.cmd 'colorscheme catppuccin'
-end
-vim.api.nvim_create_user_command('CatppuccinMocha', themes.catppuccin, {})
-
-themes.tokyonight = function()
-  vim.cmd 'colorscheme tokyonight'
-end
-vim.api.nvim_create_user_command('TokyoNight', themes.tokyonight, {})
-
--- set random theme
 local setRandomTheme = function()
   local keys = {}
   for key, _ in pairs(themes) do
@@ -71,7 +6,3 @@ local setRandomTheme = function()
   local randomTheme = keys[math.random(1, #keys)]
   themes[randomTheme]()
 end
-
--- setRandomTheme()
-
-vim.api.nvim_create_user_command('RandomTheme', setRandomTheme, {})
