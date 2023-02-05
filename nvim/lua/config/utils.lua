2c2cc1cc08877f2a61d4cee1a4483e014dbb2582
b. vaughn sulit <bvaughnsulit@gmail.com>
2023-02-04 21:26:15 -0800

    created debounce module, used for bufferline sorting, so that bufferline is only sorted after buffer has not been changed in 2 secs

--- nvim/lua/config/utils.lua
+++ nvim/lua/config/utils.lua
@@ -5,4 +5,30 @@ M.map = function(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
+-- libuv stuff
+local timers = {}
+
+M.debounce = function(fn, ms)
+  local timer = vim.loop.new_timer()
+  -- save timer to table so that it can be closed later
+  table.insert(timers, timer)
+  return function(...)
+    local args = { ... }
+    timer:start(ms, 0, vim.schedule_wrap(function ()
+     fn(unpack(args))
+    end))
+  end
+end
+
+vim.api.nvim_create_autocmd('VimLeavePre', {
+  callback = function()
+    for _, timer in pairs(timers) do
+      if timer and not timer:is_closing() then
+        timer:close()
+      end
+    end
+  end,
+
+})
+
 return M
--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,8 +1,10 @@
+local debounce = require('config.utils').debounce
+
 return {
   'akinsho/bufferline.nvim',
   event = 'VeryLazy',
   version = 'v3.*',
-  dependencies = 'nvim-tree/nvim-web-devicons',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('bufferline').setup {
       options = {
@@ -12,14 +14,23 @@ return {
         diagnostics = false,
         show_close_icon = false,
         show_buffer_close_icons = false,
-        sort_by = function(a, b)
-          return vim.fn.getbufinfo(a.id)[1].lastused > vim.fn.getbufinfo(b.id)[1].lastused
-        end,
         max_name_length = 26,
-        tab_size = 22,
-        persist_buffer_sort = false,
+        tab_size = 20,
+        persist_buffer_sort = true,
       },
     }
+
+    local run_custom_sort = debounce(function()
+      require('bufferline').sort_buffers_by(function(a, b)
+        return vim.fn.getbufinfo(a.id)[1].lastused > vim.fn.getbufinfo(b.id)[1].lastused
+      end)
+    end, 2000)
+
+    local bufferline_sort = vim.api.nvim_create_augroup('BufferlineSort', { clear = true })
+    vim.api.nvim_create_autocmd('BufEnter', {
+      callback = run_custom_sort,
+      group = bufferline_sort,
+    })
 
     vim.keymap.set({ 'n', 'v' }, ']b', '<cmd>BufferLineCycleNext<cr>', {})
     vim.keymap.set({ 'n', 'v' }, '<a-]>', '<cmd>BufferLineCycleNext<cr>', {})
