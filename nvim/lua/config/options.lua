ce236f28cdcaee2f963186acd3968f441d03d512
b. vaughn sulit <bvaughnsulit@gmail.com>
2023-06-03 15:39:49 -0700

    disable providers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = ' '
-vim.g.material_style = 'deep ocean'
+
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 vim.opt.number = true
 vim.opt.relativenumber = false
