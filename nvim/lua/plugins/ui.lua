2f94c565a8af484be8d0c2ea9259c36772541d87
b. vaughn sulit <bvaughnsulit@gmail.com>
2023-07-18 22:58:34 -0700

    add noice

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -4,19 +4,40 @@ return {
     enabled = true,
     lazy = false,
     init = function()
-      local notify = require 'notify'
-      notify.setup {
+      local notify = require('notify')
+      notify.setup({
         timeout = 2000,
         stages = 'slide',
-        max_height = function()
-          return math.floor(vim.o.lines * 0.75)
-        end,
-        max_width = function()
-          return math.floor(vim.o.columns * 0.75)
-        end,
+        max_height = function() return math.floor(vim.o.lines * 0.75) end,
+        max_width = function() return math.floor(vim.o.columns * 0.75) end,
         top_down = false,
-      }
+      })
       vim.notify = notify
     end,
+  },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = 'cmdline_popup',
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+      },
+      presets = {
+        long_message_to_split = true,
+        lsp_doc_border = true,
+      },
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
   },
 }
