f40bad29bb8246c660bd0ffe11b2aa08cc1652e8
b. vaughn sulit <bvaughnsulit@gmail.com>
2023-10-07 12:02:33 -0700

    theme configs

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -5,15 +5,17 @@ return {
   {
     'projekt0n/github-nvim-theme',
     lazy = false,
-    priority = 1001,
+    dev = false,
+    priority = 1000,
     config = function()
+      require('github-theme').setup({})
       if not is_system_dark_mode then vim.cmd('colorscheme github_light_colorblind') end
     end,
   },
   {
     'folke/tokyonight.nvim',
     lazy = false,
-    priority = 1002,
+    priority = 1000,
     config = function()
       require('tokyonight').setup({
         styles = {
@@ -22,6 +24,37 @@ return {
         },
       })
       if is_system_dark_mode then vim.cmd('colorscheme tokyonight') end
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    priority = 1000,
+    name = 'catppuccin',
+    config = function()
+      require('catppuccin').setup({
+        dim_inactive = {
+          enabled = true,
+          shade = 'dark',
+          percentage = 0.15,
+        },
+        no_italic = true,
+        integrations = {
+          telescope = { enabled = true },
+          lsp_trouble = true,
+          treesitter = true,
+          treesitter_context = true,
+          notify = true,
+          dap = true,
+          cmp = true,
+          gitsigns = true,
+          leap = true,
+          mini = true,
+          neotree = true,
+          neotest = true,
+          illuminate = true,
+        },
+      })
     end,
   },
   {
@@ -47,29 +80,6 @@ return {
       'loctvl842/monokai-pro.nvim',
       'maxmx03/fluoromachine.nvim',
       'projekt0n/github-nvim-theme',
-      {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        config = function()
-          require('catppuccin').setup({
-            integrations = {
-              telescope = { enabled = true },
-              lsp_trouble = true,
-              treesitter = true,
-              treesitter_context = true,
-              notify = true,
-              dap = true,
-              cmp = true,
-              gitsigns = true,
-              leap = true,
-              mini = true,
-              neotree = true,
-              neotest = true,
-              illuminate = true,
-            },
-          })
-        end,
-      },
     },
     opts = {
       default = {
