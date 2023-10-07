a0be1ac07d5c8d3e4c88c026cf099ab5750a3aa8
b. vaughn sulit <bvaughnsulit@gmail.com>
2023-10-07 12:20:04 -0700

    update indentblankline

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,22 +1,16 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
   event = 'BufReadPost',
+  main = 'ibl',
   dependencies = 'nvim-treesitter/nvim-treesitter',
   config = function()
-    require('indent_blankline').setup({
-      indent_blankline_char = '▏',
-      indent_blankline_context_char = '▏',
-      space_char_blankline = ' ',
-      show_current_context = false,
-      show_current_context_start = true,
-      use_treesitter = true,
-      use_treesitter_scope = false,
-      show_trailing_blankline_indent = false,
-      show_first_indent_level = true,
+    require('ibl').setup({
+      indent = {
+        char = '▏',
+      },
+      scope = {
+        enabled = false,
+      },
     })
-
-    -- unclear why including these options in the setup above doesn't work
-    vim.cmd("let g:indent_blankline_char='▏'")
-    vim.cmd("let g:indent_blankline_context_char='▏'")
   end,
 }
