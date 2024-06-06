e43db91cc88b65a05aa5289c2be81c9a16d51ad1
b. vaughn sulit <bvaughnsulit@gmail.com>
2024-06-06 08:58:48 -0700

    fix trouble error

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,11 +3,11 @@ return {
   event = 'BufReadPost',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('trouble').setup {
+    require('trouble').setup({
       position = 'bottom', -- position of the list can be: bottom, top, left, right
       height = 10, -- height of the trouble list when position is top or bottom
       width = 50, -- width of the list when position is left or right
-      icons = true, -- use devicons for filenames
+      -- icons = true, -- use devicons for filenames
       mode = 'document_diagnostics', -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
       fold_open = '', -- icon used for open folds
       fold_closed = '', -- icon used for closed folds
@@ -49,7 +49,7 @@ return {
         other = '﫠',
       },
       use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
-    }
+    })
 
     vim.keymap.set('n', '<leader>da', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
   end,
