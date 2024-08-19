6f465d9de3e8a13ffa65742839ce6e11b2438222
b. vaughn sulit <bvaughnsulit@gmail.com>
2024-08-19 10:41:39 -0700

    move marks spec

--- nvim/lua/plugins/specs.lua
+++ nvim/lua/plugins/specs.lua
@@ -62,32 +62,6 @@ return {
     },
   },
   {
-    'chentoast/marks.nvim',
-    event = 'VeryLazy',
-    config = true,
-    keys = {
-      {
-        "]'",
-        '<cmd>lua require("marks").next()<CR>',
-        { desc = 'Jump to next mark' },
-      },
-      {
-        "['",
-        '<cmd>lua require("marks").prev()<CR>',
-        { desc = 'Jump to previous mark' },
-      },
-      {
-        '<leader>mm',
-        mode = { 'n' },
-        function()
-          require('marks').mark_state:all_to_list()
-          vim.cmd('lopen')
-        end,
-        { desc = 'Toggle mark visual' },
-      },
-    },
-  },
-  {
     'kevinhwang91/nvim-bqf',
     event = 'VeryLazy',
   },
