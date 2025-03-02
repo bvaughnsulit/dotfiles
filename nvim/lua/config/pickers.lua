0f73d19eac908a22e9d952b60ed714679189298a
b. vaughn sulit <bvaughnsulit@gmail.com>
2025-03-02 13:05:41 -0800

    add picker fallback fns

--- nvim/lua/config/pickers.lua
+++ nvim/lua/config/pickers.lua
@@ -1,17 +1,42 @@
 local M = {}
 
 local pickers = {}
-M.register_picker = function(picker_name, functions) pickers[picker_name] = functions end
+local picker_functions = {}
+local picker_index = 1
 
-local picker = 'telescope'
-M.change_picker = function(new_picker) picker = new_picker end
-
-local call = function(fn_name, ...)
-  if pickers[picker] and pickers[picker][fn_name] then
-    pickers[picker][fn_name](...)
-  else
-    logger('Not found: ' .. picker .. ' ' .. fn_name)
+M.register_picker = function(picker_name, functions)
+  for _, name in ipairs(pickers) do
+    if name == picker_name then
+      vim.notify('Picker ' .. picker_name .. ' already registered')
+      return
+    end
   end
+
+  for fn_name, fn in pairs(functions) do
+    if type(fn) ~= 'function' or not M.maps[fn_name] then
+      vim.notify('Picker ' .. picker_name .. ' contains invalid function: ' .. fn_name)
+      return
+    end
+  end
+
+  table.insert(pickers, picker_name)
+  picker_functions[picker_name] = functions
+end
+
+M.change_picker = function(new_picker)
+  for i, name in ipairs(pickers) do
+    if name == new_picker then
+      picker_index = i
+      break
+    end
+    vim.notify('Picker ' .. new_picker .. ' not found')
+  end
+  vim.notify('Picker: ' .. pickers[picker_index])
+end
+
+M.cycle_picker = function()
+  picker_index = (picker_index + 1) > #pickers and 1 or picker_index + 1
+  vim.notify('Picker: ' .. pickers[picker_index])
 end
 
 M.maps = {
@@ -27,13 +52,28 @@ M.maps = {
   buffer_fuzzy = { '<leader>/f', 'Current Buffer Fuzzy Search' },
 }
 
+local call = function(fn_name, ...)
+  local current_picker = pickers[picker_index]
+  if picker_functions[current_picker] and picker_functions[current_picker][fn_name] then
+    picker_functions[current_picker][fn_name](...)
+  else
+    for i = 1, #pickers do
+      local fallback = pickers[i]
+      if picker_functions[fallback] and picker_functions[fallback][fn_name] then
+        picker_functions[fallback][fn_name](...)
+        return
+      end
+    end
+    logger('Not found: ' .. current_picker .. ' ' .. fn_name)
+  end
+end
+
 for name, opts in pairs(M.maps) do
   vim.keymap.set('n', opts[1], function() call(name) end, {
     desc = opts[2],
   })
 end
 
-vim.api.nvim_create_user_command('PickerTelescope', function() M.change_picker('telescope') end, {})
-vim.api.nvim_create_user_command('PickerSnacks', function() M.change_picker('snacks') end, {})
+vim.api.nvim_create_user_command('CyclePicker', function() M.cycle_picker() end, {})
 
 return M
