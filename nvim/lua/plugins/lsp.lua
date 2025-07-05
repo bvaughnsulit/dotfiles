eea4abe938a0eb655fb9d2d8067e5782f44e9281
b. vaughn sulit <bvaughnsulit@gmail.com>
2025-07-05 00:35:07 -0700

    hide diagnostic signs, color line nums only

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -50,6 +50,20 @@ return {
             inlay_hints = { enabled = false },
             diagnostics = {
                 virtual_text = false,
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = "",
+                        [vim.diagnostic.severity.WARN] = "",
+                        [vim.diagnostic.severity.INFO] = "",
+                        [vim.diagnostic.severity.HINT] = "",
+                    },
+                    numhl = {
+                        [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+                        [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+                        [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+                        [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+                    },
+                },
                 float = {
                     wrap = true,
                     max_width = 120,
