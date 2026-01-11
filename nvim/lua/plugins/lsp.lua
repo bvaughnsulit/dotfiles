fcabbada15c1a0d202690f863c37181d642b173f
b. vaughn sulit <bvaughnsulit@gmail.com>
2026-01-10 17:38:44 -0800

    lsp cleanup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,177 +1,3 @@
-local LazyVim = {}
-
----@alias lsp.Client.filter {id?: number, bufnr?: number, name?: string, method?: string, filter?:fun(client: lsp.Client):boolean}
-
----@param opts? lsp.Client.filter
-function LazyVim.get_clients(opts)
-    local ret = {} ---@type vim.lsp.Client[]
-    if vim.lsp.get_clients then
-        ret = vim.lsp.get_clients(opts)
-    else
-        ---@diagnostic disable-next-line: deprecated
-        ret = vim.lsp.get_active_clients(opts)
-        if opts and opts.method then
-            ---@param client vim.lsp.Client
-            ret = vim.tbl_filter(
-                function(client) return client.supports_method(opts.method, { bufnr = opts.bufnr }) end,
-                ret
-            )
-        end
-    end
-    return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
-end
-
----@param on_attach fun(client:vim.lsp.Client, buffer)
----@param name? string
-function LazyVim.on_attach(on_attach, name)
-    return vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-            local buffer = args.buf ---@type number
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            if client and (not name or client.name == name) then return on_attach(client, buffer) end
-        end,
-    })
-end
-
----@type table<string, table<vim.lsp.Client, table<number, boolean>>>
-LazyVim._supports_method = {}
-
-function LazyVim.setup()
-    local register_capability = vim.lsp.handlers["client/registerCapability"]
-    vim.lsp.handlers["client/registerCapability"] = function(err, res, ctx)
-        ---@diagnostic disable-next-line: no-unknown
-        local ret = register_capability(err, res, ctx)
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-        if client then
-            for buffer in pairs(client.attached_buffers) do
-                vim.api.nvim_exec_autocmds("User", {
-                    pattern = "LspDynamicCapability",
-                    data = { client_id = client.id, buffer = buffer },
-                })
-            end
-        end
-        return ret
-    end
-    LazyVim.on_attach(LazyVim._check_methods)
-    LazyVim.on_dynamic_capability(LazyVim._check_methods)
-end
-
----@param client vim.lsp.Client
-function LazyVim._check_methods(client, buffer)
-    -- don't trigger on invalid buffers
-    if not vim.api.nvim_buf_is_valid(buffer) then return end
-    -- don't trigger on non-listed buffers
-    if not vim.bo[buffer].buflisted then return end
-    -- don't trigger on nofile buffers
-    if vim.bo[buffer].buftype == "nofile" then return end
-    for method, clients in pairs(LazyVim._supports_method) do
-        clients[client] = clients[client] or {}
-        if not clients[client][buffer] then
-            if client.supports_method and client.supports_method(method, { bufnr = buffer }) then
-                clients[client][buffer] = true
-                vim.api.nvim_exec_autocmds("User", {
-                    pattern = "LspSupportsMethod",
-                    data = { client_id = client.id, buffer = buffer, method = method },
-                })
-            end
-        end
-    end
-end
-
----@param fn fun(client:vim.lsp.Client, buffer):boolean?
----@param opts? {group?: integer}
-function LazyVim.on_dynamic_capability(fn, opts)
-    return vim.api.nvim_create_autocmd("User", {
-        pattern = "LspDynamicCapability",
-        group = opts and opts.group or nil,
-        callback = function(args)
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            local buffer = args.data.buffer ---@type number
-            if client then return fn(client, buffer) end
-        end,
-    })
-end
-
----@param method string
----@param fn fun(client:vim.lsp.Client, buffer)
-function LazyVim.on_supports_method(method, fn)
-    LazyVim._supports_method[method] = LazyVim._supports_method[method] or setmetatable({}, { __mode = "k" })
-    return vim.api.nvim_create_autocmd("User", {
-        pattern = "LspSupportsMethod",
-        callback = function(args)
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            local buffer = args.data.buffer ---@type number
-            if client and method == args.data.method then return fn(client, buffer) end
-        end,
-    })
-end
-
----@return _.lspconfig.options
-function LazyVim.get_config(server)
-    local configs = require("lspconfig.configs")
-    return rawget(configs, server)
-end
-
----@return {default_config:lspconfig.Config}
-function LazyVim.get_raw_config(server)
-    local ok, ret = pcall(require, "lspconfig.configs." .. server)
-    if ok then return ret end
-    return require("lspconfig.server_configurations." .. server)
-end
-
-function LazyVim.is_enabled(server)
-    local c = LazyVim.get_config(server)
-    return c and c.enabled ~= false
-end
-
----@param server string
----@param cond fun( root_dir, config): boolean
-function LazyVim.disable(server, cond)
-    local util = require("lspconfig.util")
-    local def = LazyVim.get_config(server)
-    ---@diagnostic disable-next-line: undefined-field
-    def.document_config.on_new_config = util.add_hook_before(
-        def.document_config.on_new_config,
-        function(config, root_dir)
-            if cond(root_dir, config) then config.enabled = false end
-        end
-    )
-end
-
-LazyVim.action = setmetatable({}, {
-    __index = function(_, action)
-        return function()
-            vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                    only = { action },
-                    diagnostics = {},
-                },
-            })
-        end
-    end,
-})
-
----@class LspCommand: lsp.ExecuteCommandParams
----@field open? boolean
----@field handler? lsp.Handler
-
----@param opts LspCommand
-function LazyVim.execute(opts)
-    local params = {
-        command = opts.command,
-        arguments = opts.arguments,
-    }
-    if opts.open then
-        require("trouble").open({
-            mode = "lsp_command",
-            params = params,
-        })
-    else
-        return vim.lsp.buf_request(0, "workspace/executeCommand", params, opts.handler)
-    end
-end
-
 ---@type table<string, vim.lsp.Config>
 local servers = {
     lua_ls = {
@@ -296,11 +122,6 @@ return {
                 vim.lsp.buf.rename,
                 desc = "Rename",
             },
-            {
-                "<leader>cA",
-                LazyVim.action.source,
-                desc = "Source Action",
-            },
         },
         config = function()
             vim.diagnostic.config({
@@ -332,7 +153,7 @@ return {
             })
 
             ---@param client vim.lsp.Client
-            local on_attach = function(client, buffer)
+            local on_attach = function(client, _buffer)
                 if client and client.name == "eslint" then
                     client.server_capabilities.documentFormattingProvider = true
                 elseif client and client.name == "vtsls" then
@@ -349,9 +170,25 @@ return {
                 vim.cmd([[hi! default link Folded LspInlayHint]])
             end
 
-            LazyVim.on_attach(on_attach)
-            LazyVim.on_dynamic_capability(on_attach)
-            LazyVim.setup()
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if client then return on_attach(client, args.buf) end
+                end,
+            })
+
+            -- i dunno if this is necessary
+            local register_capability = vim.lsp.handlers["client/registerCapability"]
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.lsp.handlers["client/registerCapability"] = function(err, res, ctx)
+                local client = vim.lsp.get_client_by_id(ctx.client_id)
+                if client then
+                    for buffer in pairs(client.attached_buffers) do
+                        on_attach(client, buffer)
+                    end
+                end
+                return register_capability(err, res, ctx)
+            end
 
             for name, config in pairs(servers) do
                 local disabled = { "basedpyright" }
