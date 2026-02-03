114374766c0487991bef09a662b9bcc8e2ee0996
b. vaughn sulit <bvaughnsulit@gmail.com>
2026-02-02 20:01:21 -0800

    fzf lua

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -4,21 +4,43 @@ return {
     {
         "https://github.com/ibhagwan/fzf-lua",
         event = "VeryLazy",
-        enabled = false,
+        enabled = true,
         dependencies = {
             "https://github.com/nvim-tree/nvim-web-devicons",
         },
         ---@module "fzf-lua"
         ---@type fzf-lua.Config|{}
         ---@diagnostic disable: missing-fields
-        opts = {},
+        opts = {
+            fzf_opts = {
+                ["--cycle"] = true,
+            },
+            winopts = {
+                height = 0.95,
+                width = 0.95,
+                preview = {
+                    flip_columns = 200,
+                },
+            },
+        },
         ---@diagnostic enable: missing-fields
         config = function(_, opts)
             local fzf_lua = require("fzf-lua")
             fzf_lua.setup(opts)
 
             require("config.pickers").register_picker("fzf-lua", {
+                find_files = function() fzf_lua.files() end,
+                buffers = function() fzf_lua.buffers() end,
                 live_grep = function() fzf_lua.live_grep() end,
+                lsp_definitions = function() fzf_lua.lsp_definitions() end,
+                lsp_references = function() fzf_lua.lsp_references() end,
+                lsp_type_definitions = function() fzf_lua.lsp_typedefs() end,
+                keymaps = function() fzf_lua.keymaps() end,
+                help_tags = function() fzf_lua.helptags() end,
+                commands = function() fzf_lua.commands() end,
+                buffer_fuzzy = function() fzf_lua.lgrep_curbuf() end,
+                pickers = function() fzf_lua.builtin() end,
+                resume = function() fzf_lua.resume() end,
             })
 
             vim.keymap.set("n", "<leader>ppf", ":FzfLua<cr>", { desc = "Fzf Lua Pickers" })
