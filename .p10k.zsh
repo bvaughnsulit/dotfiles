facb6b899ec55c8d5b22cb6ec3a28a4e71a275aa
b. vaughn sulit <bvaughnsulit@gmail.com>
2026-03-04 18:49:18 -0800

    prompt bg jobs

--- .p10k.zsh
+++ .p10k.zsh
@@ -432,11 +432,11 @@
 
   #######################[ background_jobs: presence of background jobs ]#######################
   # Don't show the number of background jobs.
-  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=false
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=true
   # Background jobs color.
   typeset -g POWERLEVEL9K_BACKGROUND_JOBS_FOREGROUND=70
   # Custom icon.
-  # typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VISUAL_IDENTIFIER_EXPANSION='󰀿'
 
   #######################[ direnv: direnv status (https://direnv.net/) ]########################
   # Direnv color.
