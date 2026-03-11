31e7d922085782056b92dc80c50d8bb1de09d872
b. vaughn sulit <bvaughnsulit@gmail.com>
2026-03-10 18:50:24 -0700

    comment out fs

--- .claude/on_pre_tool_use.ts
+++ .claude/on_pre_tool_use.ts
@@ -1,9 +1,13 @@
+const fs = require("node:fs")
+
 ;(function on_pre_tool_use() {
     const input = process.stdin.on("data", data => {
         const input = JSON.parse(data.toString())
         const command = input.tool_input.command
 
-        const allowedCommands = ["ls", "echo", "cat"]
+        // const file = fs.openSync("/Users/vaughn/dotfiles/log.txt", "a")
+        // fs.writeSync(file, `Received command: ${command}\n`)
+        // fs.closeSync(file)
 
         const result = {
             hookSpecificOutput: {
