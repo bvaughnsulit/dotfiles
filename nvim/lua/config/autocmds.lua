d5915d3c1442731af3d0a4564f41cbf54e2dd6bc
b. vaughn sulit <bvaughnsulit@gmail.com>
2026-07-24 17:11:13 -0700

    markdown conceal

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -30,7 +30,7 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function()
         vim.opt_local.wrap = true
         vim.opt_local.spell = false
-        vim.opt_local.conceallevel = 0
+        vim.opt_local.conceallevel = 2
     end,
 })
 
